e879d1042a1f8fb757859c1c68c5411c2a09a2cf
Quantum-Cucumber <quantumcucumber@protonmail.com>
2022-10-19 12:34:29 +1100

    Pronouns page integration

--- cogs/general.py
+++ cogs/general.py
@@ -2,8 +2,9 @@ import discord
 from discord.ext import commands
 import datetime as dt
 from utils import BOOL_OPTIONS, create_bar
-from config import SPOTIFY_EMOJI, BOOSTER_ROLE, GUILD_ID
+from config import SPOTIFY_EMOJI, BOOSTER_ROLE, GUILD_ID, PRIMARY
 from math import floor
+from db import db
 
 # Maps to minutes
 DATE_SCALE = {
@@ -18,6 +19,18 @@ BREATHE_GIFS = {
     "geometric": "https://tenor.com/view/breathe-in-breathe-iout-calming-calm-down-gif-12208363",
     "gauge": "https://www.duffthepsych.com/wp-content/uploads/2016/07/478Breathe500x500c129revised.gif"
 }
+
+PRONOUNS_PAGE_URL = "https://en.pronouns.page/api/profile/get/{}?version=2"
+
+PRONOUNS_PAGE_BASE_OPINIONS = {
+    "yes": "❤️",
+    "meh": "👍",
+    "close": "🫂",
+    "jokingly": "😛",
+    "no": "👎",
+}
+
+PRONOUNS_PAGE_FLAG_SERVERS = [830277756058337341, 830294210371518474, 932944778801315880]
 
 
 def duration_to_str(duration: dt.timedelta):
@@ -152,6 +165,135 @@ class General(commands.Cog):
         embed.set_footer(text=f"User ID: {user.id}")
 
         await ctx.respond(embed=embed)
+
+    pronouns = discord.SlashCommandGroup(name="pronouns")
+
+    @pronouns.command()
+    async def set(self, ctx: discord.ApplicationContext, username: str):
+        """Set your pronouns.page username"""
+        url = f"https://en.pronouns.page/api/profile/get/{username}?version=2"
+
+        await ctx.defer()
+
+        async with self.bot.session.get(url) as response:
+            status_code = response.status
+
+            if status_code != 200:
+                await ctx.respond("pronouns.page cannot be reached", ephemeral=True)
+                return
+
+        await db.settings.update_one({"_id": str(ctx.user.id)}, {"$set": {"pronouns_page": username}}, upsert=True)
+
+        await ctx.respond("Username set", ephemeral=True)
+
+    @pronouns.command()
+    async def view(self, ctx: discord.ApplicationContext, user: discord.Option(discord.Member, required=False)):
+        """Display the user's pronouns.page profile"""
+        ephemeral = True
+        if not user:
+            user = ctx.user
+            ephemeral = False
+
+        await self.send_pronouns_profile(ctx, user, ephemeral)
+
+    @discord.user_command(name="View Pronouns")
+    async def user_pronouns(self, ctx: discord.ApplicationContext, member: discord.Member):
+        await self.send_pronouns_profile(ctx, member)
+
+    async def send_pronouns_profile(self, ctx: discord.ApplicationContext, user: discord.Member, ephemeral=True):
+        await ctx.defer(ephemeral=ephemeral)
+
+        entry = await db.settings.find_one({"_id": str(user.id), "pronouns_page": {"$ne": None}},
+                                           projection={"pronouns_page": True})
+
+        if not entry:
+            if ctx.user.id == user.id:
+                # TODO: When slash group mention fixed:
+                #  embed = discord.Embed(colour=PRIMARY, description=self.set.mention)
+                await ctx.respond("You have not setup your pronouns.page profile")
+            else:
+                await ctx.respond("That user has not setup their pronouns.page profile")
+            return
+
+        # At this point, the user does have a pronouns.page username set
+        username = entry["pronouns_page"]
+
+        async with self.bot.session.get(PRONOUNS_PAGE_URL.format(username)) as response:
+            status_code = response.status
+
+            if status_code != 200:
+                await ctx.respond("pronouns.page cannot be reached")
+                return
+
+            data = await response.json()
+
+        if "en" not in data["profiles"]:
+            await ctx.respond("That user does not have an english profile set up")
+            return
+
+        embed = self.pronouns_page_embed(data)
+
+        await ctx.respond(embed=embed)
+
+    def pronouns_page_embed(self, data: dict):
+        profile = data["profiles"]["en"]
+
+        src_username = data["username"]
+        avatar = data["avatar"]
+        page_url = "https://en.pronouns.page/@" + src_username
+
+        embed = discord.Embed(colour=PRIMARY)
+        embed.set_author(name=src_username + "🔗", icon_url=avatar, url=page_url)
+
+        if profile["description"] != "":
+            embed.description = profile["description"]
+
+        opinions = PRONOUNS_PAGE_BASE_OPINIONS.copy()
+
+        for key, values in profile["opinions"].items():
+            opinions.update({
+                key: "[{}]".format(values["description"])
+            })
+
+        names = ""
+        for entry in profile["names"]:
+            name = entry["value"]
+            opinion = opinions[entry["opinion"]]
+            names += f"{opinion} {name}\n"
+
+        if names:
+            embed.add_field(name="Names", value=names)
+
+        pronouns = ""
+        for entry in profile["pronouns"]:
+            name = entry["value"]
+            opinion = opinions[entry["opinion"]]
+            pronouns += f"{opinion} {name}\n"
+
+        if pronouns:
+            embed.add_field(name="Pronouns", value=pronouns)
+
+        if profile["flags"]:
+            # Grab all flag emojis
+            flag_emojis = []
+            for guild_id in PRONOUNS_PAGE_FLAG_SERVERS:
+                flag_emojis += self.bot.get_guild(guild_id).emojis
+
+            flags = []
+            for flag in profile["flags"]:
+                emoji = discord.utils.get(flag_emojis, name=flag.replace(" ", ""))
+                if emoji:
+                    flags.append(str(emoji))
+
+            if flags:
+                embed.add_field(name="Flags", value=" ".join(flags))
+
+        if profile["links"]:
+            embed.add_field(name="Links", value="\n".join(profile["links"]), inline=False)
+
+        embed.set_footer(text="pronouns.page")
+
+        return embed
 
 
 def setup(bot: discord.Bot):
