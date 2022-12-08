a69e3a45f651a666e21c5da7fd0246b723beea08
Quantum-Cucumber <quantumcucumber@protonmail.com>
2022-12-08 17:54:51 +1100

    Auto-update staff list

--- cogs/passive.py
+++ cogs/passive.py
@@ -1,5 +1,5 @@
 import discord
-from config import SUGGESTIONS_ID, LIKE_EMOJI, DISLIKE_EMOJI
+from config import SUGGESTIONS_ID, LIKE_EMOJI, DISLIKE_EMOJI, STAFF_LIST_MESSAGE, STAFF_ROLE, GUILD_ID
 
 
 class Passive(discord.Cog):
@@ -15,6 +15,24 @@ class Passive(discord.Cog):
             if first_message:
                 await first_message.add_reaction(DISLIKE_EMOJI)
                 await first_message.add_reaction(LIKE_EMOJI)  # Last emoji shows up on the post
+
+    @discord.Cog.listener()
+    async def on_member_update(self, old_member: discord.Member, new_member: discord.Member):
+        if old_member.roles != new_member.roles:
+            # Roles updated
+            guild = self.bot.get_guild(GUILD_ID)
+            staff_role = guild.get_role(STAFF_ROLE)
+
+            if staff_role in old_member.roles or staff_role in new_member.roles:
+                description = "\n".join([member.mention for member in staff_role.members])
+
+                channel = self.bot.get_guild(STAFF_LIST_MESSAGE[0]).get_channel(STAFF_LIST_MESSAGE[1])
+                message = await channel.fetch_message(STAFF_LIST_MESSAGE[2])
+                embed = message.embeds[0].copy()
+
+                embed.description = description
+
+                await message.edit(embed=embed)
 
 
 def setup(bot):
