afe7f73133f68e8288c9bf8bdc85c31217ca2fb2
Quantum-Cucumber <quantumcucumber@protonmail.com>
2022-12-08 17:41:51 +1100

    Edit embeds

--- cogs/embeds.py
+++ cogs/embeds.py
@@ -20,7 +20,7 @@ class Embed(discord.Cog):
         if not channel:
             channel = ctx.channel
 
-        embed = discord.Embed(title=title, description=description.replace("\\n", "\n"))
+        embed = discord.Embed(title=title, description=description.replace("\\n", "\n") if description else None)
         if colour:
             try:
                 # ctx is literally unused, so it doesn't matter
@@ -42,6 +42,48 @@ class Embed(discord.Cog):
             await ctx.respond(f"Unable to send embed\n```{e}```", ephemeral=True)
         else:
             await ctx.respond("Sent", ephemeral=True)
+
+    @discord.slash_command()
+    async def edit_embed(self, ctx: discord.ApplicationContext,
+                         message: discord.Message,
+                         title: discord.Option(str, required=False),
+                         description: discord.Option(str, required=False),
+                         image_url: discord.Option(str, required=False),
+                         colour: discord.Option(str, required=False),
+                         timestamp: discord.Option(int, required=False, choices=BOOL_OPTIONS) = 0
+                         ):
+        """Send an embed"""
+        if message.author.id != self.bot.user.id:
+            await ctx.respond("Cannot edit messages sent by other bots", ephemeral=True)
+            return
+
+        embed = discord.Embed()
+        if message.embeds:
+            embed = message.embeds[0].copy()
+
+        if title:
+            embed.title = title
+        if description:
+            embed.description = description.replace("\\n", "\n")
+        if colour:
+            try:
+                # ctx is literally unused, so it doesn't matter
+                # noinspection PyTypeChecker
+                embed.colour = await commands.ColourConverter().convert(None, colour)
+            except commands.BadArgument:
+                await ctx.respond("Unknown colour", ephemeral=True)
+                return
+        if image_url:
+            embed.set_image(url=image_url)
+        if timestamp:
+            embed.timestamp = discord.utils.utcnow()
+
+        try:
+            await message.edit(embed=embed)
+        except discord.DiscordException as e:
+            await ctx.respond(f"Unable to send embed\n```{e}```", ephemeral=True)
+        else:
+            await ctx.respond("Edited", ephemeral=True)
 
 
 def setup(bot):
